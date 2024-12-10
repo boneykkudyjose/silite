c6e7573edd247fe0dcc8a02d981d9eb8ad7116ef
boneykkudyjose <boney_jose@yahoo.com>
2024-12-09 16:47:31 -0800

    reporting date

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 
-from supabase_client import fetch_top_holdings
+from supabase_client import fetch_top_holdings, get_reporting_period_list
 
 
 # Streamlit App
 st.title("Top Holdings Heatmap")
-
+reporting_date=get_reporting_period_list()
 # Input for report_date
-report_date = st.text_input("Enter the report date (YYYY-MM-DD):", "2024-06-30")
+report_date = st.text_input("Enter the report date (YYYY-MM-DD):", reporting_date[0])
 
 if st.button("Fetch and Plot"):
     # Fetch data
@@ -24,10 +24,37 @@ if st.button("Fetch and Plot"):
         st.dataframe(df)
 
       # Convert 'counter' values to integers for the heatmap
-        pivot_data = df.pivot_table(index="security_name", columns="ticker", values="counter", fill_value=0).astype(int)
+        import math
+    import numpy as np
+    # heatmap shape: 10 rows, 10 columns
+    shape = (10, 10) 
+    total_number_of_funds = 3005
+    df['CounterRelative'] = [round(counter/(total_number_of_funds )*100,1) for counter in df['counter']]
 
-        # Plot heatmap
-        fig, ax = plt.subplots(figsize=(10, 8))
-        sns.heatmap(pivot_data, annot=True, fmt="d", cmap="coolwarm", ax=ax)
-        ax.set_title("Top Holdings Heatmap")
-        st.pyplot(fig)
+    tickers = np.asarray(df['ticker']).reshape(shape)
+    counters = np.asarray(df['CounterRelative']).reshape(shape)
+
+    df['Position'] = range(1,len(df) + 1)
+    df['y_rows'] = [(math.floor(x/shape[1]) + 1 if x%shape[1] != 0 else math.floor(x/shape[1])) for x in df['Position']]
+    df['x_cols'] = [(x%shape[1] if x%shape[1] != 0 else shape[1]) for x in df['Position']]
+
+    pivot_table = df.pivot(index='y_rows', columns='x_cols', values='CounterRelative')
+    fig, ax = plt.subplots(figsize=(18, 7))
+
+    title = f"Top 100 Holdings Distribution Across 13F Filings, {reporting_date}"
+    heatmap_labels = np.asarray(["{0} \n {1}%".format(ticker, counter) 
+                              for ticker, counter in zip(tickers.flatten(), counters.flatten())]).reshape(shape)
+
+
+    plt.title(title, fontsize=18)
+    ttl = ax.title
+    ttl.set_position([0.5, 1.05])
+
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    ax.axis('off')
+
+    sns.heatmap(pivot_table, annot=heatmap_labels, fmt="", cmap="BuGn", linewidths=0.30, ax=ax)
+
+    st.pyplot(fig)
--- supabase_client.py
+++ supabase_client.py
@@ -21,4 +21,22 @@ def fetch_top_holdings(period_of_report):
         return pd.DataFrame()  # Return an empty DataFrame in case of an error
 
 # Call the function with the desired period_of_report
-print (fetch_top_holdings("2024-06-30"))
+# print (fetch_top_holdings("2024-06-30"))
+
+
+def get_reporting_period_list(cik=None):
+    # Call Supabase function
+    response = supabase.rpc("get_reporting_period_list", {"cik_input": cik}).execute()
+    #print(response.data)
+    if response:
+        rows = response.data
+        reporting_periods = pd.DataFrame(rows, columns=['period_of_report'])
+        latest_two_periods = reporting_periods.sort_values(by='period_of_report', ascending=False).head(2)
+        latest_two_periods['period_of_report'] = pd.to_datetime(latest_two_periods['period_of_report'], errors='coerce')
+        str_date = latest_two_periods['period_of_report'].dt.strftime('%Y-%m-%d')
+        return str_date.tolist()
+    else:
+        print(f"Error: {response.error_message}")
+        return []
+
+print(get_reporting_period_list())
